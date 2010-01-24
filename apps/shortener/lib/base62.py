8ae94f7e6d4a1c0c4f1335c2793b2783574e21f7
Fred Wenzel <fwenzel@mozilla.com>
2010-01-24 23:35:16 +0100

    base62 encoding

---
+++ apps/shortener/lib/base62.py
@@ -0,0 +1,27 @@
+"""to-and-from base 62 encoding"""
+import string
+
+CHARS = string.ascii_lowercase + string.ascii_uppercase + string.digits
+BASE = len(CHARS)
+
+def to62(number):
+    """encodes a positive integer in base 62."""
+    n = int(number)
+    assert(n > 0)
+
+    result = []
+    while n:
+        (n, r) = divmod(n, BASE)
+        result.append(CHARS[r])
+    result.reverse()
+    return "".join(result) or CHARS[0]
+
+def from62(base62str):
+    """decodes a base 62 encoded string to base 10"""
+    num = 0
+    for i in range(len(base62str)):
+        digit = CHARS.find(base62str[-i-1])
+        assert(digit >= 0)
+        num += digit * (62**i)
+    return num
+
