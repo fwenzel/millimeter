1cc1d5f5aa89db543935dc880c5dc90c8aebdaff
Fred Wenzel <fwenzel@mozilla.com>
2010-01-26 00:08:21 +0100

    base template and awesome new fonts

--- settings.py
+++ settings.py
@@ -50,7 +50,7 @@ MEDIA_URL = '/media/'
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+ADMIN_MEDIA_PREFIX = '/admin-media/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '### change me ###'
@@ -90,3 +90,4 @@ try:
     from local_settings import *
 except ImportError:
     pass
+
--- urls.py
+++ urls.py
@@ -1,13 +1,23 @@
+from django.conf import settings
 from django.conf.urls.defaults import *
 from django.contrib import admin
+from django.views.generic.simple import direct_to_template
 
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # (r'^millimeter/', include('millimeter.foo.urls')),
-
+    (r'^$', direct_to_template, {'template': 'index.html'}, 'index'),
     (r'^admin/', include(admin.site.urls)),
-
     (r'^(?P<slug>\w+)/$', 'shortener.views.forward'),
 )
+
+# serve media files in debug mode
+if settings.DEBUG:
+    # Remove leading and trailing slashes so the regex matches.
+    media_url = settings.MEDIA_URL.lstrip('/').rstrip('/')
+    urlpatterns += patterns('',
+        (r'^%s/(?P<path>.*)$' % media_url, 'django.views.static.serve',
+         {'document_root': settings.MEDIA_ROOT}),
+    )
+
