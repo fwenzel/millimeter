c67bab49b554047826dd582e971463d27f45a511
Fred Wenzel <fwenzel@mozilla.com>
2010-01-25 10:40:58 +0100

    two-line forwarding script

--- apps/shortener/views.py
+++ apps/shortener/views.py
@@ -1,5 +1,11 @@
-from django.http import Http404
+from django.http import HttpResponsePermanentRedirect
+from django.shortcuts import get_object_or_404
+
+from models import Link
+
 
 def forward(request, slug):
-    raise Http404
+    """The actual forwarder magic"""
+    link = get_object_or_404(Link, slug=slug)
+    return HttpResponsePermanentRedirect(link.url)
 
