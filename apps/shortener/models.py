bf5fb60b746d7d6d913b0653f95ac7a424c09220
Fred Wenzel <fwenzel@mozilla.com>
2010-01-25 20:22:19 +0100

    auto-save slug in model instead of listening for post-save signal

--- apps/shortener/models.py
+++ apps/shortener/models.py
@@ -1,14 +1,13 @@
 from django.contrib.auth.models import User
 from django.db import models, IntegrityError
 
-from fields import CharNullField
 from lib.base62 import to62
 
 
 class Link(models.Model):
     """Model representing a single shortened URL"""
     url = models.URLField(max_length=255)
-    slug = CharNullField(
+    slug = models.CharField(
         blank=True,
         db_index=True,
         default=None,
@@ -27,18 +26,17 @@ class Link(models.Model):
     def __unicode__(self):
         return "%s: %s" % (self.slug, self.url)
 
+    def save(self, *args, **kwargs):
+        """if the user did not choose a slug, use an auto-generated one"""
+        if self.slug:
+            super(Link, self).save(*args, **kwargs)
+            return
 
-def check_slug(sender, instance, created, **kwargs):
-    """if the user did not choose a slug, use an auto-generated one"""
-    if instance.slug:
-        return
-    newslug = instance.autoslug()
-    while True:
-        instance.slug = newslug
-        try:
-            instance.save()
-            break
-        except IntegrityError: # if taken, add an underscore
-            newslug += '_'
-models.signals.post_save.connect(check_slug, sender=Link)
+        self.slug = self.autoslug()
+        while True:
+            try:
+                super(Link, self).save(*args, **kwargs)
+                return
+            except IntegrityError: # if taken, add an underscore
+                self.slug += '_'
 
