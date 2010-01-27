a0b9265c0b10c12c1d5aa564630dc2a0dbdf9910
Fred Wenzel <fwenzel@mozilla.com>
2010-01-27 11:35:16 +0100

    Ensure user-provided slug only contains valid characters (issue 3)

--- apps/shortener/forms.py
+++ apps/shortener/forms.py
@@ -1,4 +1,8 @@
+import re
+
 from django import forms
+
+from lib.base62 import CHARS as BASE62_CHARS
 
 from models import Link
 
@@ -7,4 +11,14 @@ class ShortenForm(forms.ModelForm):
     class Meta:
         model = Link
         exclude = ('user',)
+
+    def clean_slug(self):
+        """Make sure slugs only contain valid characters"""
+        char_regex = r'^[%s]*$' % BASE62_CHARS
+        value = self.cleaned_data['slug']
+        if not re.match(char_regex, value):
+            raise forms.ValidationError(
+                'Short names must only contain alphanumeric characters.'
+            )
+        return value
 
