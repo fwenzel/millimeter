81433694ad31f8ed26d0d836525af451a7182c4e
Fred Wenzel <fwenzel@mozilla.com>
2010-01-30 12:12:19 +0100

    auto-generate slug independently from URL id, jump over already assigned ones (issue 8)

--- apps/shortener/models.py
+++ apps/shortener/models.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.models import User
-from django.db import models, IntegrityError
+from django.db import IntegrityError, models, transaction
 
-from fields import CharNullField
-from lib.base62 import to62
+from lib import base62
+
+from .fields import CharNullField
 
 
 class Link(models.Model):
@@ -18,12 +19,10 @@ class Link(models.Model):
         unique=True,
         verbose_name='short name',
     )
-    user = models.ForeignKey(User)
+    is_autoslug = models.BooleanField(
+        default=False, help_text='was this short name auto-generated?')
+    users = models.ManyToManyField(User)
     visited = models.PositiveIntegerField(default=0, editable=False)
-
-    def autoslug(self):
-        """return id-based url slug"""
-        return to62(self.id)
 
     def __unicode__(self):
         return "%s: %s" % (self.slug, self.url)
@@ -34,11 +33,35 @@ class Link(models.Model):
         if self.slug:
             return
 
-        self.slug = self.autoslug()
+        self.is_autoslug = True
         while True:
+            self.slug = AutoSlug.next_autoslug()
             try:
                 super(Link, self).save(*args, **kwargs)
                 return
-            except IntegrityError: # if taken, add an underscore
-                self.slug += '_'
+            except IntegrityError: # if taken, try again
+                pass
+
+
+class AutoSlug(models.Model):
+    """
+    Auto-generates unique IDs
+
+    A link in need of a new slug will add a field to AutoSlug, obtain its ID,
+    then delete that field. All databases' sequence feature will take care of
+    these IDs being unique.
+    """
+    # we define no fields, so Django's id / pk field will be the only one
+
+    @classmethod
+    def next_autoslug(cls):
+        """return new unique url slug"""
+        iditem = cls()
+        iditem.save(force_insert=True)
+        newslug = base62.to62(iditem.pk)
+        # we can delete all but the highest entry, but need to keep this one
+        # around because Django does not use real auto_increment columns on all
+        # databases (SQLite, for example). :(
+        cls.objects.filter(pk__lt=iditem.pk).delete()
+        return newslug
 
